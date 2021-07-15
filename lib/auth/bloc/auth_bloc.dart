f6ca90603a08bd399cbb4d4d0d2c7a3fbc008648
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-14 23:53:17 -0400

    created auth bloc

--- lib/auth/bloc/auth_bloc.dart
+++ lib/auth/bloc/auth_bloc.dart
@@ -3,16 +3,62 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:users_repository/users_repository.dart';
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:school_notifier/app/app.dart';
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial());
+  AuthBloc(this._appBloc) : super(AuthInitial()) {
+    _appBlocSubscription = _appBloc.stream.listen(_mapAppBlocStateToAuthEvent);
+  }
+
+  final AppBloc _appBloc;
+  late StreamSubscription _appBlocSubscription;
 
   @override
-  Stream<AuthState> mapEventToState(
-    AuthEvent event,
-  ) async* {
-    // TODO: implement mapEventToState
+  Stream<AuthState> mapEventToState(AuthEvent event) async* {
+    if (event is AuthParentSignedIn) {
+      yield await _mapAuthParentSignedInToState(event);
+    } else if (event is AuthStarted) {
+      yield await _mapAuthStartedToState(event);
+    } else if (event is AuthNewParent) {
+      yield await _mapAuthNewParentState(event);
+    }
+  }
+
+  Future<AuthParentSignInSuccess> _mapAuthParentSignedInToState(
+      AuthParentSignedIn event) async {
+    // await Future.delayed(Duration(seconds: 0));
+    return AuthParentSignInSuccess(
+        parent: event.parent, teacher: event.teacher, user: event.user);
+  }
+
+  Future<AuthInitial> _mapAuthStartedToState(AuthStarted event) async {
+    // await Future.delayed(Duration(seconds: 0));
+    return AuthInitial(
+        parent: event.parent, teacher: event.teacher, user: event.user);
+  }
+
+  Future<AuthNewParentSetup> _mapAuthNewParentState(AuthNewParent event) async {
+    // await Future.delayed(Duration(seconds: 0));
+    return AuthNewParentSetup(
+        parent: event.parent, teacher: event.teacher, user: event.user);
+  }
+
+  Future<void> _mapAppBlocStateToAuthEvent(AppState appState) async {
+    if (appState.status == AppStatus.parent) {
+      add(AuthParentSignedIn(parent: appState.parent, user: appState.user));
+    } else if (appState.status == AppStatus.newParent) {
+      add(AuthNewParent(parent: appState.parent, user: appState.user));
+    } else {
+      add(AuthStarted(user: appState.user, parent: appState.parent));
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _appBlocSubscription.cancel();
+    return super.close();
   }
 }
--- lib/auth/bloc/auth_event.dart
+++ lib/auth/bloc/auth_event.dart
@@ -1,18 +1,31 @@
 part of 'auth_bloc.dart';
 
 abstract class AuthEvent extends Equatable {
-  AuthEvent({this.parent, this.teacher});
-
-  Parent? parent;
-  Teacher? 
-
+  AuthEvent({this.user, this.parent, this.teacher});
+  final User? user;
+  final Parent? parent;
+  final Teacher? teacher;
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [user, parent, teacher];
 }
 
-class AuthTeacherSignedIn extends AuthEvent {}
+class AuthTeacherSignedIn extends AuthEvent {
+  AuthTeacherSignedIn({required teacher, parent, user})
+      : super(teacher: teacher, parent: parent, user: user);
+}
 
-class AuthParentSignedIn extends AuthEvent {}
+class AuthParentSignedIn extends AuthEvent {
+  AuthParentSignedIn({required parent, teacher, user})
+      : super(teacher: teacher, parent: parent, user: user);
+}
 
-class AuthStarted extends AuthEvent {}
+class AuthNewParent extends AuthEvent {
+  AuthNewParent({required parent, teacher, user})
+      : super(teacher: teacher, parent: parent, user: user);
+}
+
+class AuthStarted extends AuthEvent {
+  AuthStarted({parent, teacher, user})
+      : super(teacher: teacher, parent: parent, user: user);
+}
