4b7d0218c1839447fcc5db6a4c311cd928e16c63
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-20 16:18:29 -0400

    deleted subscription bloc

--- lib/event_repository_test/EventViewModel.dart
+++ lib/event_repository_test/EventViewModel.dart
@@ -8,26 +8,13 @@ class EventViewModel {
   // .getAllSubscribedEvents(['eventSubId', 'eventSubId2']),
 
   /// returns the entire movies list with user-favourite information
-  Stream<List<FirestoreEvent>> combineToStream() {
-    return Rx.combineLatest2(database.getSingleStream('eventSubId'),
-        database.getSingleStream('eventSubId2'),
-        (List<FirestoreEvent> movies, List<FirestoreEvent> userFavourites) {
-      return (movies + userFavourites);
-    });
-    // return Rx.combineLatest2(
-    //     database.moviesStream(), database.userFavouritesStream(),
-    //     (List<Movie> movies, List<UserFavourite> userFavourites) {
-    //   return movies.map((movie) {
-    //     final userFavourite = userFavourites?.firstWhere(
-    //         (userFavourite) => userFavourite.movieId == movie.id,
-    //         orElse: () => null);
-    //     return MovieUserFavourite(
-    //       movie: movie,
-    //       isFavourite: userFavourite?.isFavourite ?? false,
-    //     );
-    //   }).toList();
-    // });
-  }
+  // Stream<List<FirestoreEvent>> combineToStream() {
+  //   return Rx.combineLatest2(database.getSingleStream('eventSubId'),
+  //       database.getSingleStream('eventSubId2'),
+  //       (List<FirestoreEvent> movies, List<FirestoreEvent> userFavourites) {
+  //     return (movies + userFavourites);
+  //   });
+  // }
 
   Stream<List<FirestoreEvent>> combineAllStreams() {
 
