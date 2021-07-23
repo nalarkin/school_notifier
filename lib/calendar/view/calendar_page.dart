8e4e8f4374718f5a1ee5a4cb8f06e51ec97bf6e3
James <jnpark1280@gmail.com>
2021-07-23 01:20:00 -0400

    initial push

---
+++ lib/calendar/view/calendar_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:school_notifier/authentication/authentication.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({Key? key}) : super(key: key);
+
+  @override
+  _CalendarPageState createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDate = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calendar'),
+        actions: <Widget>[
+          IconButton(
+            key: const Key('homePage_logout_iconButton'),
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: () => context
+                .read<AuthenticationBloc>()
+                .add(AuthenticationLogoutRequested()),
+          )
+        ],
+      ),
+      body: TableCalendar(
+        firstDay: DateTime.utc(2014, 03, 01),
+        lastDay: DateTime.utc(2030, 10, 30),
+        focusedDay: _focusedDate,
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDate = focusedDay;
+            });
+          }
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          _focusedDate = focusedDay;
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:school_notifier/app/app.dart';
+import 'package:school_notifier/calendar/view/calendar_page.dart';
 import 'package:users_repository/users_repository.dart';
 import 'package:flutter/rendering.dart';
 
@@ -15,8 +17,10 @@ void main() async {
   final firestoreParentsRepository = FirestoreParentsRepository();
   // final postsRepository = FirestorePostsRepository();
   await authenticationRepository.user.first;
-  runApp(App(
-    authenticationRepository: authenticationRepository,
-    firestoreParentsRepository: firestoreParentsRepository,
-  ));
+  runApp(MaterialApp(home: CalendarPage())
+      //App(
+      //authenticationRepository: authenticationRepository,
+      // firestoreParentsRepository: firestoreParentsRepository,
+      //)
+      );
 }
