ceedb0c6e469842e32c0304c92541a3215d02e19
James <jnpark1280@gmail.com>
2021-07-26 19:00:20 -0400

    added splash screen

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:school_notifier/app/app.dart';
-import 'package:school_notifier/calendar/view/calendar_create_event_page.dart';
-import 'package:school_notifier/calendar/view/calendar_page.dart';
 import 'package:school_notifier/event_repository_test/event_page.dart';
 import 'package:school_notifier/firestore_user_debug/firestore_page.dart';
 import 'package:school_notifier/home/home.dart';
@@ -30,7 +28,6 @@ Map<String, WidgetBuilder> allRoutes = <String, WidgetBuilder>{
   ProfilePage.routeName: (context) => ProfilePage(),
   EventPage.routeName: (context) => EventPage(),
   TokenPage.routeName: (context) => TokenPage(),
-  CalendarPage.routeName: (context) => CalendarPage(),
   ConversationPage.routeName: (context) => ConversationPage(),
   ConversationDebug.routeName: (context) => ConversationDebug(),
   MessagePage.routeName: (context) => MessagePage(),
