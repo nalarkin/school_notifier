5ac1275674bd0a3ab9fc4032b5f4557269034754
Drew Hartman <drewdavishartman@gmail.com>
2021-07-28 12:37:18 -0400

    got notifications working (static)

--- lib/notification/notification_service.dart
+++ lib/notification/notification_service.dart
@@ -1,40 +1,57 @@
 import 'package:event_repository/event_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:school_notifier/event_repository_test/event_page.dart';
+import 'package:school_notifier/home/view/home_page.dart';
 import 'package:school_notifier/main.dart';
 
-class NotificationService {
-  Future<void> init() async {
-    final AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('app_icon');
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
-    final InitializationSettings initializationSettings =
-        InitializationSettings(
-            android: initializationSettingsAndroid, iOS: null, macOS: null);
+String? selectedNotificationPayload;
 
-    // await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-    //     onSelectNotification: selectNotification);
+final BehaviorSubject<String?> selectNotificationSubject =
+    BehaviorSubject<String?>();
 
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onSelectNotification: (String? payload) async {
-      if (payload != null) {
-        debugPrint('notification payload: $payload');
-      }
-    });
+int id = 0;
+String? title = "Event Title";
+String? body = "Event Description";
+String? payload = "data";
+
+Future<void> init() async {
+  final NotificationAppLaunchDetails? notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  String initialRoute = HomePage.routeName;
+  if (notificationAppLaunchDetails?.didNotificationLaunchApp ?? false) {
+    selectedNotificationPayload = notificationAppLaunchDetails!.payload;
+    initialRoute = EventPage.routeName;
   }
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('app_icon');
+
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid, iOS: null, macOS: null);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: showNotification);
 }
 
-Future<void> selectNotification(String? payload) async {
-  // handles notification tapped logic here
+Future<void> showNotification(String? payload) async {
+  const AndroidNotificationDetails androidPlatformChannelSpecifics =
+      AndroidNotificationDetails(
+          'channel id', 'channel name', 'channel description',
+          importance: Importance.max, priority: Priority.high);
 
-  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-      'test', 'School Notifier', 'Here is a test notification',
-      importance: Importance.max, priority: Priority.max, showWhen: false);
-
-  var platformChannelSpecifics =
+  const NotificationDetails platformChannelSpecifics =
       NotificationDetails(android: androidPlatformChannelSpecifics);
 
   await flutterLocalNotificationsPlugin.show(
-      0, 'plain title', 'plain body', platformChannelSpecifics,
-      payload: 'data');
+    id,
+    title,
+    body,
+    platformChannelSpecifics,
+    payload: payload,
+  );
+  print("Notification Sent");
 }
