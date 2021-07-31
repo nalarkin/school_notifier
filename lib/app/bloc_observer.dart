715ba518e68d3786d12d40b6b882f74ce561acde
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-30 23:04:35 -0400

    removed verbose print statements

--- lib/app/bloc_observer.dart
+++ lib/app/bloc_observer.dart
@@ -2,11 +2,11 @@
 import 'package:bloc/bloc.dart';
 
 class AppBlocObserver extends BlocObserver {
-  @override
-  void onEvent(Bloc bloc, Object? event) {
-    super.onEvent(bloc, event);
-    print(event);
-  }
+  // @override
+  // void onEvent(Bloc bloc, Object? event) {
+  //   super.onEvent(bloc, event);
+  //   print(event);
+  // }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
@@ -14,15 +14,15 @@ class AppBlocObserver extends BlocObserver {
     super.onError(bloc, error, stackTrace);
   }
 
-  @override
-  void onChange(BlocBase bloc, Change change) {
-    super.onChange(bloc, change);
-    print(change);
-  }
+  // @override
+  // void onChange(BlocBase bloc, Change change) {
+  //   super.onChange(bloc, change);
+  //   print(change);
+  // }
 
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    super.onTransition(bloc, transition);
-    print(transition);
-  }
+  // @override
+  // void onTransition(Bloc bloc, Transition transition) {
+  //   super.onTransition(bloc, transition);
+  //   print(transition);
+  // }
 }
