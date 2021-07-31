130dd33d39ad29960de138e3e679ef6ec30c0942
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-30 23:19:02 -0400

    removed debug page from drawer

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -9,97 +9,66 @@ import 'package:school_notifier/subscriptions/view/subscriptions_page.dart';
 
 Widget customDrawer(BuildContext context) {
   return Drawer(
-      // Add a ListView to the drawer. This ensures the user can scroll
-      // through the options in the drawer if there isn't enough vertical
-      // space to fit everything.
-
-      child: ListView(
-
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-        DrawerHeader(
-          decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-          child: Text(
-            'Quick Actions',
-            style: Theme.of(context).textTheme.headline6,
-            // ?.copyWith(color: kOnPrimaryColor),
-          ),
+      child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+    DrawerHeader(
+      decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+      child: Text(
+        'Quick Actions',
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    ),
+    ListTile(
+        title: Text('Logout'),
+        leading: Icon(
+          Icons.logout,
+          color: Colors.black,
         ),
-        ListTile(
-            title: Text('Logout'),
-            leading: Icon(
-              Icons.logout,
-              color: Colors.black,
-            ),
-            onTap: () {
-              Navigator.pushNamedAndRemoveUntil(
-                  context, '/', (Route<dynamic> route) => false);
-              context
-                  .read<AuthenticationBloc>()
-                  .add(AuthenticationLogoutRequested());
-            }),
-
-        // ListTile(
-        //   title: Text('Settings'),
-        //   leading: Icon(
-        //     Icons.settings,
-        //     color: Colors.black,
-        //   ),
-        //   onTap: () {
-        //     // Update the state of the app
-        //     // ...
-        //     // Then close the drawer
-        //     Navigator.pop(context);
-        //     Navigator.pushNamed(context, SettingsPage.routeName);
-        //   },
-        // ),
-        ListTile(
-            title: Text('Profile'),
-            leading: Icon(
-              Icons.face,
-              color: Colors.black,
-            ),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-              Navigator.pushNamed(context, ProfilePage.routeName);
-            }),
-
-        ListTile(
-            title: Text('Conversations'),
-            leading: Icon(
-              Icons.mail,
-              color: Colors.black,
-            ),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, ConversationPage.routeName);
-            }),
-        ListTile(
-            title: Text('Debug Links'),
-            leading: Icon(
-              Icons.bug_report,
-              color: Colors.black,
-            ),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, HomePage.routeName);
-            }),
-        ListTile(
-            title: Text('Home'),
-            leading: Icon(
-              Icons.home,
-              color: Colors.black,
-            ),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-              Navigator.pushNamed(context, SubscriptionPage.routeName);
-            }),
-      ]));
+        onTap: () {
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/', (Route<dynamic> route) => false);
+          context
+              .read<AuthenticationBloc>()
+              .add(AuthenticationLogoutRequested());
+        }),
+    ListTile(
+        title: Text('Profile'),
+        leading: Icon(
+          Icons.face,
+          color: Colors.black,
+        ),
+        onTap: () {
+          Navigator.pop(context);
+          Navigator.pushNamed(context, ProfilePage.routeName);
+        }),
+    ListTile(
+        title: Text('Conversations'),
+        leading: Icon(
+          Icons.mail,
+          color: Colors.black,
+        ),
+        onTap: () {
+          Navigator.pop(context);
+          Navigator.pushNamed(context, ConversationPage.routeName);
+        }),
+    // ListTile(
+    //     title: Text('Debug Links'),
+    //     leading: Icon(
+    //       Icons.bug_report,
+    //       color: Colors.black,
+    //     ),
+    //     onTap: () {
+    //       Navigator.pop(context);
+    //       Navigator.pushNamed(context, HomePage.routeName);
+    //     }),
+    ListTile(
+        title: Text('Home'),
+        leading: Icon(
+          Icons.home,
+          color: Colors.black,
+        ),
+        onTap: () {
+          Navigator.pop(context);
+          Navigator.pushNamed(context, SubscriptionPage.routeName);
+        }),
+  ]));
 }
